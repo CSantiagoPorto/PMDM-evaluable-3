e05a3eea2824a27b7d6a6aa504241221f311bfb5
CSantiagoPorto <148074839+CSantiagoPorto@users.noreply.github.com>
2025-04-17 14:22:47 +0200

    Intentnado vincular el Adapter. No le gusta el import

---
+++ app/src/main/java/com/example/ligasfragment/model/Liga.kt
@@ -0,0 +1,7 @@
+package com.example.ligasfragment.model
+
+class Liga (val nombre: String) {
+
+    //Paso así el nombre porque quiero que pase la propiedad directamente al crea el objeto
+
+}
--- app/src/main/java/com/example/ligasfragment/ui/fragment/MainFragment.kt
+++ app/src/main/java/com/example/ligasfragment/ui/fragment/MainFragment.kt
@@ -5,8 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.ligasfragment.databinding.FragmentLoginBinding
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.example.ligasfragment.databinding.FragmentMainBinding
+import com.example.ligasfragment.model.Liga
+import com.example.ligasfragment.ui.adapter.LigaAdapter
+
+
+
+
 
 class MainFragment: Fragment() {
 
@@ -14,6 +23,8 @@ class MainFragment: Fragment() {
     //esta variable me dará acceso a todos los elementos de fragmant_login.xml porque
     //FrafmentLogingBinding representa mu layout
     //Esot crea una "puerta" de acceso directo a mi diseño para poder usar sus componentes
+    private val listaLigas = ArrayList<Liga>()
+
 
     //Método que asocia la parte gráfica con la parte lógica
     override fun onCreateView(
@@ -25,6 +36,26 @@ class MainFragment: Fragment() {
         //Procedemos a darle valor al binding.
         //Infla el diseño fragment_login.xml y guárdalo en la cariable para que pueda usar sus vistas
         binding=FragmentMainBinding.inflate(inflater,container,false)
+        var adapter=com.example.ligasfragment.ui.fragment.LigaAdapter(listaLigas)
+        //Esto va a configurar el RecyclerView para que use el adaptador
         return binding.root
+     //   var adapter =LigaAdapter(listaLigas)
     }
+
+
+
+   /* private fun cargarLigasAPI(){
+        val url= "https://www.thesportsdb.com/api/v1/json/3/all_leagues.php"
+        val colaPeticiones = Volley.newRequestQueue(requireContext())
+        //Necesito una cola de peticiones para nuestros amigos GET y POST
+        val peticion= JsonObjectRequest(Request.Method.GET,//le indico método
+            url,//le doy la url
+            null,//de primeras vacío, por tanto null
+            {respuesta->
+                val arrayLigas=respuesta.getJSONObject(i)
+
+            })
+
+
+    }*/
 }
