d0f6b6c2043c64b3449b79c711f35092a8d10788
CSantiagoPorto <148074839+CSantiagoPorto@users.noreply.github.com>
2025-04-25 10:17:14 +0200

    Modificado el item liga para que cumpla parámentro

--- app/src/main/java/com/example/ligasfragment/LigasAdapter.kt
+++ app/src/main/java/com/example/ligasfragment/LigasAdapter.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ligasfragment.model.Liga
@@ -18,12 +17,11 @@ class LigasAdapter(
 
     interface OnFavoritoClickListener {
         fun onFavoritoClick(liga: Liga, position: Int)
-        fun onLigaClick(liga:Liga)
+        fun onLigaClick(liga: Liga)
     }
 
     class LigaViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val nombreTextView: TextView    = itemView.findViewById(R.id.NombreLiga)//este es el textView donde voy a mostrar la liga
-        val favoritoImagen: ImageView = itemView.findViewById(R.id.imageViewFavorito)
+        val nombreTextView: TextView = itemView.findViewById(R.id.NombreLiga)
         val verLigaButton: Button = itemView.findViewById(R.id.btnLiga)
     }
 
@@ -39,29 +37,8 @@ class LigasAdapter(
         val liga = listaLigas[position]
         holder.nombreTextView.text = liga.nombre
 
-        // miro si está en favoritos
-        val favs = sharedPreferences
-            .getStringSet("favoritos", emptySet())!!
-        val estrellas = if (favs.contains(liga.nombre)) R.drawable.fav else R.drawable.no
-
-
-       // holder.favoritoImageView.setOnClickListener(listener.onFavoritoClick(liga,position))
-
-       // Glide.with(holder.itemView)
-        //    .load(liga.logoUrl)
-          //  .placeholder(R.drawable.no)
-            //.into(holder.favoritoImageView)
-        holder.favoritoImagen.setImageResource(estrellas)
-        holder.favoritoImagen.setOnClickListener {
-            listener.onFavoritoClick(liga, position)
-
-
-        }
-        //Este es el callback del botón
         holder.verLigaButton.setOnClickListener {
             listener.onLigaClick(liga)
-
-
         }
     }
 }
